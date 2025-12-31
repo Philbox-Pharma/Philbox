52fe5a114a8556cbcc36f8bea66a7871236c3206
shah541-g <syedabdulalishah.786@gmail.com>
2025-12-31 20:04:27 +0500

    nodemailer changed to tls

--- server/src/main/config/nodemailer.config.js
+++ server/src/main/config/nodemailer.config.js
@@ -5,8 +5,8 @@ dotenv.config();
 
 export const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com', // or your SMTP host
-  port: 465, // 465 for secure, 587 for TLS
-  secure: true, // true for 465, false for 587
+  port: 587, // 465 for secure, 587 for TLS
+  secure: false, // true for 465, false for 587
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
